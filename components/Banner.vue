f10aa58de29dbec374892a1f7e461cd6852293e2
jtorricoc <80930073+jtorricoc@users.noreply.github.com>
2021-03-24 00:32:04 -0400

    sitio web

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,7 +1,5 @@
 <template>
-
   <div>
-
     <section class="banner-section banner-one">
       <div class="left-based-text">
         <div class="base-inner">
@@ -13,83 +11,91 @@
           </div>
           <div class="social-links">
             <ul class="clearfix">
-              <li><a href="#"><span>Twitter</span></a></li>
-              <li><a href="#"><span>Facebook</span></a></li>
-              <li><a href="#"><span>Youtube</span></a></li>
+              <li>
+                <a href="#"><span>Twitter</span></a>
+              </li>
+              <li>
+                <a href="#"><span>Facebook</span></a>
+              </li>
+              <li>
+                <a href="#"><span>Youtube</span></a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
 
       <div class="banner-carousel">
-
-        <no-ssr> <!-- important to add no-ssr-->
+        <no-ssr>
+          <!-- important to add no-ssr-->
           <carousel :items="1">
-        <!-- Slide Item -->
-        <div class="slide-item">
-          <div class="image-layer" style="background-image: url(/images/main-slider/1.jpg);"></div>
-          <div class="left-top-line"></div>
-          <div class="right-bottom-curve"></div>
-          <div class="right-top-curve"></div>
-          <div class="auto-container">
-            <div class="content-box">
-              <div class="content">
-                <div class="inner">
-                  <div class="sub-title">welcome to Linoor agency</div>
-                  <h1>Smart Web <br>Design Agency</h1>
-                  <div class="link-box">
-                    <nuxt-link class="theme-btn btn-style-one" to="/about">
-                      <i class="btn-curve"></i>
-                      <span class="btn-title">Discover More</span>
-                    </nuxt-link>
+            <!-- Slide Item -->
+            <div class="slide-item">
+              <div
+                class="image-layer"
+                style="
+                  background-image: url(/images/main-slider/contaduria.jpg);
+                "
+              ></div>
+              <div class="left-top-line"></div>
+              <div class="right-bottom-curve"></div>
+              <div class="right-top-curve"></div>
+              <div class="auto-container">
+                <div class="content-box">
+                  <div class="content">
+                    <div class="inner">
+                      <div class="sub-title">welcome to Linoor agency</div>
+                      <h1>Smart Web <br />Design Agency</h1>
+                      <div class="link-box">
+                        <nuxt-link class="theme-btn btn-style-one" to="/about">
+                          <i class="btn-curve"></i>
+                          <span class="btn-title">Discover More</span>
+                        </nuxt-link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
 
-        <!-- Slide Item -->
-        <div class="slide-item">
-          <div class="image-layer" style="background-image: url(/images/main-slider/2.jpg);"></div>
-          <div class="left-top-line"></div>
-          <div class="right-bottom-curve"></div>
-          <div class="right-top-curve"></div>
-          <div class="auto-container">
-            <div class="content-box">
-              <div class="content">
-                <div class="inner">
-                  <div class="sub-title">welcome to Linoor agency</div>
-                  <h1>Smart Web <br>Design Agency</h1>
-                  <div class="link-box">
-                    <nuxt-link class="theme-btn btn-style-one" to="/about">
-                      <i class="btn-curve"></i>
-                      <span class="btn-title">Discover More</span>
-                    </nuxt-link>
+            <!-- Slide Item -->
+            <div class="slide-item">
+              <div
+                class="image-layer"
+                style="background-image: url(/images/main-slider/impresora.png)"
+              ></div>
+              <div class="left-top-line"></div>
+              <div class="right-bottom-curve"></div>
+              <div class="right-top-curve"></div>
+              <div class="auto-container">
+                <div class="content-box">
+                  <div class="content">
+                    <div class="inner">
+                      <div class="sub-title">welcome to Linoor agency</div>
+                      <h1>Smart Web <br />Design Agency</h1>
+                      <div class="link-box">
+                        <nuxt-link class="theme-btn btn-style-one" to="/about">
+                          <i class="btn-curve"></i>
+                          <span class="btn-title">Discover More</span>
+                        </nuxt-link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
           </carousel>
         </no-ssr>
-
       </div>
     </section>
-
   </div>
-
 </template>
 
 <script>
-
-  export default {
-    name: "Banner",
-  }
+export default {
+  name: "Banner",
+};
 </script>
 
 <style scoped>
-
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,26 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.png' },
-      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap" },
+      { rel: "icon", type: "image/x-icon", href: "/images/favicon.png" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap"
+      },
       { rel: "stylesheet", href: "/css/bootstrap.css" },
       { rel: "stylesheet", href: "/css/animate.css" },
       { rel: "stylesheet", href: "/css/fontawesome-all.css" },
@@ -29,43 +36,39 @@ export default {
 
     script: [
       { src: "/plugins/glightbox/glightbox.min.js", body: true },
-      { src: "/plugins/accordion/accordion.min.js", body: true },
+      { src: "/plugins/accordion/accordion.min.js", body: true }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#ffa915', height: '4px' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#ffa915", height: "4px" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    {src: 'plugins/owl.js', ssr: false},
-    {src: 'plugins/mixitup.js', ssr: false},
+    { src: "plugins/owl.js", ssr: false },
+    { src: "plugins/mixitup.js", ssr: false }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
